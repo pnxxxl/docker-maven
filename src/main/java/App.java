0f90e20551468310c57d88a616fd12d62ead6faa
pnxxxl <dashasir555@gmail.com>
2024-11-27 13:53:43 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Maven inside a Docker container!");
+        Thread.sleep(5000); // Ждем 5 секунд
+        System.out.println("Application finished.");
     }
 }
+
